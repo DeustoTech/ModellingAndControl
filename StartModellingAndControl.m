90adba12850c8385550aed7b26f6497f7e91943d
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2021-07-21 10:13:23 +0200

    Update StartModellingAndControl.m

--- StartModellingAndControl.m
+++ StartModellingAndControl.m
@@ -1,12 +1,33 @@
+clear 
+
+name_file = 'StartModellingAndControl.m';
+main_path = which(name_file);
+main_path = replace(main_path,name_file,'');
+
+cd(main_path)
+%% clean 
+try 
+    rmdir('data') 
+catch 
+    
+end
+try 
+    rmdir('src/dependences')
+catch
+end
+%% Make a new folders
+
+data_path = fullfile(main_path,'data');
+data_depen = fullfile(main_path,'src/dependences');
+%
+mkdir(data_path)
+mkdir(data_depen)
+
+%% download HortiMED data 
+unzip('https://github.com/djoroya/HortiMED-Data-Sources/archive/refs/heads/main.zip',data_path)
+
+%% download AGRO SOFC data
 
 
-main_path = which('StartModellingAndControl.m');
-main_path = replace(main_path,'StartModellingAndControl.m','');
-mkdir(fullfile(main_path,'Data'))
-
-%%
-
-r = websave('Data/DATASOURCES.zip','https://drive.google.com/u/0/uc?id=1OiTq6zIqg1yiTl_EkcKuH7O7hGFUqhmK&export=download');
-unzip('Data/DATASOURCES.zip','Data/')
-
+%% add all files 
 addpath(genpath(pwd))
