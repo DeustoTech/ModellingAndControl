6f90a6205601b514cbbe7dd3314aaa9f5020bef8
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2021-09-02 15:01:50 +0200

    Update StartModellingAndControl.m

--- StartModellingAndControl.m
+++ StartModellingAndControl.m
@@ -28,6 +28,24 @@ unzip('https://github.com/djoroya/HortiMED-Data-Sources/archive/refs/heads/main.
 
 %% download AGRO SOFC data
 
+%%
+try
+    casadi.SX.sym('s');
+catch
+    disp('CasADi will be download ...')
+    casADi_folder = fullfile(main_path,'src','dependences','CasADi');
+    if ~exist(casADi_folder,'dir')
+        mkdir(casADi_folder)
+    end
+    if ismac
+        untar('https://github.com/casadi/casadi/releases/download/3.5.1/casadi-osx-matlabR2015a-v3.5.1.tar.gz',casADi_folder)
+    elseif ispc
+        unzip('https://github.com/casadi/casadi/releases/download/3.5.1/casadi-windows-matlabR2016a-v3.5.1.zip',casADi_folder)
+    elseif isunix
+        untar('https://github.com/casadi/casadi/releases/download/3.5.1/casadi-linux-matlabR2014b-v3.5.1.tar.gz',casADi_folder)
+    end
+    addpath(genpath(pathfile))
 
+end
 %% add all files 
 addpath(genpath(pwd))
